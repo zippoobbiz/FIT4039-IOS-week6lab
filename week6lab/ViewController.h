624d41c27cbf01ca17bbddecfcdf79e49389d7d3
Xiaoduo <zippoobbiz@gmail.com>
2015-04-14 19:57:46 +1000

    Initial Commit

---
+++ week6lab/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  week6lab
+//
+//  Created by BigBadEgg on 4/14/15.
+//  Copyright (c) 2015 Xiaoduo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
